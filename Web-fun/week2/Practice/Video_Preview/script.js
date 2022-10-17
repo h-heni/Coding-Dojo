38e0d0b03f24fe9f20eca7f4535f6942f63877b4
h-heni <h.heni@hotmail.com>
2022-10-17 14:39:39 +0100

    initial commit

---
+++ Web-fun/week2/Practice/Video_Preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+function onover(element){
+    element.play();
+    element.muted=true;
+}
+
+function outover(element){
+    element.pause();
+    element.muted=true;
+
+}
