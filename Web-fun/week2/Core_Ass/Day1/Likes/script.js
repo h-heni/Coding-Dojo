d5e521405de4910b4a4eda076a59c97836e74df0
h-heni <h.heni@hotmail.com>
2022-10-17 21:27:46 +0100

    initial commit

---
+++ Web-fun/week2/Core_Ass/Day1/Likes/script.js
@@ -0,0 +1,25 @@
+// function plusOne (element){
+//     var i=document.querySelector("add")
+//     i++
+// }
+// var elementnum= document.querySelector(".add")
+var count=9
+var count1=12
+var count2=9
+
+function plusOne (){
+    count++
+    document.querySelector(".add").innerText=count +" "+ "likes"
+}
+
+function plusOne1 (){
+    count1++
+    document.querySelector(".add1").innerText=count1 +" "+ "likes"
+}
+function plusOne2 (){
+    count2++
+    document.querySelector(".add2").innerText=count2 +" "+ "likes"
+}
+
+
+
