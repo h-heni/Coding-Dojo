554fb12d4376c7a05eb4668e222a1d1facc9abfe
h-heni <h.heni@hotmail.com>
2022-10-19 17:34:12 +0100

    initial commit

---
+++ Web-fun/Heni_BeltExam/script.js
@@ -0,0 +1,10 @@
+var pop=document.querySelector(".pop-up")
+function del (){
+    pop.remove()
+}
+function change (element){
+    element.src = "./images/succulents-2.jpg";
+}
+function back(element) {
+    element.src= "./images/succulents-1.jpg";
+} 
